eca20ebdf79e9bb96809a249eeaeeec9c1cbe269
Christzxz <christnarvarte15@gmail.com>
2024-01-03 18:29:47 +0800

    add server url

--- server/config/corsOption.js
+++ server/config/corsOption.js
@@ -1,4 +1,4 @@
 module.exports = corsOptions = {
-  origin: "http://localhost:3000", // Replace with your frontend URL
+  origin: `${process.env.LIVE || process.env.LOCAL}`, // Replace with your frontend URL
   credentials: true, // Allow credentials (cookies)
 };
