cc99e9408c79f6d7831bc56890aaef262fe712cc
Christzxz <christnarvarte15@gmail.com>
2024-01-03 18:53:12 +0800

    try one if will work

--- nextjs-todolist/app/api/axios.js
+++ nextjs-todolist/app/api/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `${
-    "https://notetify-server.onrender.com/" || "http://localhost:3001"
-  }`,
+  baseURL: "https://notetify-server.onrender.com/",
   withCredentials: true,
 });
