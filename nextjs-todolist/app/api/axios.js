40ca4836b41d0ec4bd76c9656c316c8e9fb412cb
Christzxz <christnarvarte15@gmail.com>
2024-01-03 18:33:43 +0800

    add live url

--- nextjs-todolist/app/api/axios.js
+++ nextjs-todolist/app/api/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: `${
+    "https://notetify-server.onrender.com/" || "http://localhost:3001"
+  }`,
   withCredentials: true,
 });
