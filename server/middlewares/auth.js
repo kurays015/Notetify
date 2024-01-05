92d16e4fe960fac3b322dfd462fa8a8a8f5ca6e8
Christzxz <126612073+kurays015@users.noreply.github.com>
2024-01-05 18:34:09 +0800

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,14 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = async function requireAuth(req, res, next) {
-  const token = req.cookies?.accessToken || req.cookies["accessToken"];
-  try {
-    if (!token) {
-      throw new Error("Unauthorize, no token found");
+   try {
+    const authorizationHeader = req.headers['authorization'];
+
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+      throw new Error('Unauthorized, no token found');
     }
+
+    const token = authorizationHeader.split(' ')[1];
+
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       if (err) {
-        throw new Error("Invalid credentials");
+        throw new Error('Invalid credentials');
       }
       req.user = { userId: decoded.user_id };
       next();
