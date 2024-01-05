f5ca14cebd83bfe1b175d970c9179067cb2cf1ed
Christzxz <126612073+kurays015@users.noreply.github.com>
2024-01-05 18:42:06 +0800

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = async function requireAuth(req, res, next) {
    try {
     const authorizationHeader = req.headers['authorization'];
-
+   console.log(authorizationHeader)
     if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
       throw new Error('Unauthorized, no token found');
     }
